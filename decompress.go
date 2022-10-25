d2eec1a232698546d35d6461517665d72b055a74
Martin Lindhe <martin@ubique.se>
2022-10-25 20:24:40 +0200

    Decompress: check for out of bounds read and return error

--- decompress.go
+++ decompress.go
@@ -1,12 +1,17 @@
 package lz77
 
-func Decompress(compress_data []byte) []byte {
+import "fmt"
+
+func Decompress(compress_data []byte) ([]byte, error) {
 	raw_data := make([]byte, 0)
 	for i := 0; i < len(compress_data); i += 3 {
 		pos := len(raw_data) - int(compress_data[i])
 		length := pos + int(compress_data[i+1])
+		if length < 0 || pos < 0 || pos+length > len(compress_data) {
+			return nil, fmt.Errorf("lz77: out of bounds read. i %d, pos %d, length %d", i, pos, length)
+		}
 		raw_data = append(raw_data, raw_data[pos:length]...)
 		raw_data = append(raw_data, compress_data[i+2])
 	}
-	return raw_data
+	return raw_data, nil
 }
